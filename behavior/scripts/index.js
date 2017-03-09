eb731b6d4d5a6ab1b6e4b1e82533a43ab7f386a2
Subhash Kadam <subhash.kadam@praxify.com>
2017-03-09 16:06:16 +0530

    test

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,7 +1,6 @@
 'use strict'
 
-exports.handle = (client) => {
-  // Create steps
+exports.handle = function handle(client) {
   const sayHello = client.createStep({
     satisfied() {
       return Boolean(client.getConversationState().helloSent)
@@ -13,11 +12,9 @@ exports.handle = (client) => {
         documentation_link: 'http://docs.init.ai',
       })
       client.addResponse('provide/instructions')
-
       client.updateConversationState({
         helloSent: true
       })
-
       client.done()
     }
   })
@@ -29,48 +26,43 @@ exports.handle = (client) => {
 
     prompt() {
       client.addResponse('apology/untrained')
-      client.done()
+     client.done()
     }
   })
 
   const handleGreeting = client.createStep({
-  satisfied() {
-    return false
-  },
+    satisfied() {
+      return false
+    },
 
-  prompt() {
-    client.addResponse('greeting')
-    client.done()
-  }
+    prompt() {
+      client.addResponse('greeting')
+      client.done()
+    }
   })
 
   const handleGoodbye = client.createStep({
-  satisfied() {
-    return false
-  },
+    satisfied() {
+      return false
+    },
 
-  prompt() {
-    client.addResponse('goodbye')
-    client.done()
-  }
-})
+    prompt() {
+      client.addResponse('goodbye')
+     client.done()
+    }
+  })
 
   client.runFlow({
     classifications: {
-      // map inbound message classifications to names of streams
-goodbye: 'goodbye',
-    greeting: 'greeting',
-    },
-    autoResponses: {
-      // configure responses to be automatically sent as predicted by the machine learning model
+      goodbye: 'goodbye',
+      greeting: 'greeting'
     },
     streams: {
- goodbye: handleGoodbye,
-    greeting: handleGreeting,
- greeting: handleGreeting,
+      goodbye: handleGoodbye,
+      greeting: handleGreeting,
       main: 'onboarding',
       onboarding: [sayHello],
-      end: [untrained],
-    },
+      end: [untrained]
+    }
   })
 }
